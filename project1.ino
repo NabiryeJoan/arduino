2fdfe53fc1116ccbd00d507e7424bb6d61315082
Joan Nabirye <107198755+NabiryeJoan@users.noreply.github.com>
2022-10-15 12:47:30 +0300

    lighting more than one bulb and adding the sound

--- project1.ino
+++ project1.ino
@@ -1,6 +1,11 @@
+
 void setup() {
   // put your setup code here, to run once:
 pinMode(11,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(5,OUTPUT);
 }
 
 void loop() {
@@ -9,4 +14,21 @@ digitalWrite(11,HIGH);
 delay(1000);
 digitalWrite(11,LOW);
 delay(1000);
+digitalWrite(8,HIGH);
+delay(1000);
+digitalWrite(8,LOW);
+delay(1000);
+digitalWrite(7,HIGH);
+delay(2000);
+digitalWrite(7,LOW);
+delay(1000);
+digitalWrite(6,HIGH);
+delay(1000);
+digitalWrite(6,LOW);
+delay(1000);
+digitalWrite(5,HIGH);
+delay(1000);
+digitalWrite(5,LOW);
+delay(1000);
+
 }
